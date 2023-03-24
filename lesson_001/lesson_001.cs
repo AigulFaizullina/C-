6f3a6b89c657a2463ff9cb3e19b5de76a1953e12
Aigul <aigul00@yandex.ru>
2023-03-24 19:35:33 +0500

    Д/З 1

---
+++ lesson_001/lesson_001.cs
@@ -0,0 +1,58 @@
+﻿// Задание 1.
+// Console.Write("Введите первое число: ");
+// int num_1 = int.Parse(Console.ReadLine());
+ 
+// Console.Write("Введите второе число: ");
+// int num_2 = int.Parse(Console.ReadLine());
+ 
+// if(num_1>num_2)
+// {
+//     Console.WriteLine($"максимальное первое число: {num_1}");
+// }
+// else
+// {
+//     Console.WriteLine($"максимальное второе число: {num_2}");
+// }
+ 
+// Задание 2.
+// Console.Write("Введите первое число: ");
+// int num_1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num_2 = int.Parse(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int num_3 = int.Parse(Console.ReadLine());
+// if (num_1>num_2 && num_1>num_3)
+// {
+//     Console.WriteLine($"максимальное первое число: {num_1}");
+// }
+// else if (num_2>num_1 && num_2>num_3)
+// {
+//     Console.WriteLine($"максимальное второе число: {num_2}");
+// }
+//  else
+// {
+//     Console.WriteLine($"максимальное третье число: {num_3}");
+// }
+// Задание 3.
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine());
+// if(num%2==0)
+// {
+//     Console.WriteLine($"{num} - четное");
+// }
+// else
+// {
+//     Console.WriteLine($"{num} - нечетное");
+// }
+// Задание 4.
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine());
+// for (int i=2; i<=num; i++)
+// {
+//     if(i%2==0)
+//     {
+//         Console.Write($"{i} ");
+//     }
+// }
