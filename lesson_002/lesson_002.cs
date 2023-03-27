92ea9002904b1e2ad71ac31e022d403fe9b89c00
Aigul <aigul00@yandex.ru>
2023-03-27 19:06:38 +0500

    Д/З 2

---
+++ lesson_002/lesson_002.cs
@@ -0,0 +1,19 @@
+﻿
+// Задание 1.
+// int num = new Random().Next(100,999);
+ 
+// int a2=num%100/10;
+// Console.WriteLine($"{num} {a2}");
+ 
+// Задание 2.
+// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine());
+// if (num>0 && num<100) {
+//     Console.WriteLine("третьей цифры нет ");
+// }
+// else {
+//     while (num>999) {
+//         num=num/10;
+//     }
+//     Console.WriteLine(num%10);
+// }.
