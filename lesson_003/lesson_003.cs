175d8f2a43093a288174ece336b7b629920ea046
Aigul <aigul00@yandex.ru>
2023-03-30 19:20:17 +0500

    Д/З 3

---
+++ lesson_003/lesson_003.cs
@@ -0,0 +1,40 @@
+﻿// Задание 1.
+// Console.Clear();
+// int num=int.Parse(Console.ReadLine());
+
+
+
+// if (num/10000!=num%10){
+//     Console.WriteLine("нет");
+// }
+// else if (num/1000%10!=num%100/10){
+//     Console.WriteLine("нет");
+// }
+// else {
+//     Console.WriteLine("да");
+// }
+ 
+
+// Задание 2.
+// Console.Write("Х1: ");
+// int x1=int.Parse(Console.ReadLine());
+// Console.Write("Y1: ");
+// int y1=int.Parse(Console.ReadLine());
+// Console.Write("Z1: ");
+// int z1=int.Parse(Console.ReadLine());
+// Console.Write("Х2: ");
+// int x2=int.Parse(Console.ReadLine());
+// Console.Write("Y2: ");
+// int y2=int.Parse(Console.ReadLine());
+// Console.Write("Z2: ");
+// int z2=int.Parse(Console.ReadLine());
+// double d = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
+// Console.WriteLine($"d={d:f2}");
+ 
+
+// Задание 3.
+// Console.Write("Введите число: ");
+// int num=int.Parse(Console.ReadLine());
+// for (int i=1; i<num+1; i++){
+//     Console.Write(Math.Pow(i,3)+ " ");
+// }
