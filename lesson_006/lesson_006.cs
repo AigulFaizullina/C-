450b54652c48bb8c1fb321a745c245c4f857b1f9
Aigul <aigul00@yandex.ru>
2023-04-10 19:02:34 +0500

    Д/З 6

---
+++ lesson_006/lesson_006.cs
@@ -0,0 +1,19 @@
+﻿// Задание 1.
+// Console.Clear();
+// Console.Write("Введите числа через пробел: ");
+// int conter=0;
+// string[] array = Console.ReadLine().Split(' ');
+ 
+// for (int i=0; i<array.Length;i++){
+//     if (int.Parse(array[i])>0){
+//         conter+=1;
+//     }
+// }
+// Console.WriteLine(conter);
+ 
+// Задание 2.
+// Console.Write("Введите b1: ");
+// int b1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите k1: ");
+// int k1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите b2: ");
