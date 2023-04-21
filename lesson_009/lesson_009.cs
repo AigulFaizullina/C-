0bd2bb7d914aff0f131e4a62949882b7433383dd
Aigul <aigul00@yandex.ru>
2023-04-21 17:29:54 +0500

    Д/3 9

---
+++ lesson_009/lesson_009.cs
@@ -0,0 +1,55 @@
+﻿// Задание 1.
+// using System;
+// using static System.Console;
+// Clear();
+// Write("Введите число от: ");
+// int number_1 = int.Parse(ReadLine());
+// Write("Введите число до: ");
+// int number_2 = int.Parse(ReadLine());
+// Write(WriteNow(number_1,number_2));
+// string WriteNow(int number_1,int number_2)
+// {if (number_1==number_2){
+//     return number_2.ToString();
+// }
+//     return number_1+", "+WriteNow(--number_1,number_2);
+// }
+
+// Задание 2.
+// using System;
+// using static System.Console;
+// Clear();
+// Write("Введите число от: ");
+// int number_1 = int.Parse(ReadLine());
+// Write("Введите число до: ");
+// int number_2 = int.Parse(ReadLine());
+// Write(WriteNow(number_1,number_2));
+// int WriteNow(int number_1,int number_2)
+// {if (number_1==number_2){
+//     return number_2;
+// }
+//     return number_1+WriteNow(++number_1,number_2);
+// }
+
+// Задание 3.
+// using System;
+// using static System.Console;
+// Clear();
+// Write("Введите число M: ");
+// int m = int.Parse(ReadLine());
+// Write("Введите число N: ");
+// int n = int.Parse(ReadLine());
+// Write(Akkerman(m,n));
+// int Akkerman(int m,int n)
+// {
+//     if (m==0){
+//         return n+1;
+//     }
+//     else if (n==0 && m>0){
+//         return Akkerman(m-1,1);
+//     }
+//     else{
+//         return (Akkerman(m-1,Akkerman(m,n-1)));
+//     }
+
+
+// }
